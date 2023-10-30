3e6a2a15e1873b42b987c34d81ab73739b693374
Tammam <tammam.wafai@gmail.com>
2023-10-29 19:24:11 -0500

    Try/Catch were added to controllers/activities.js

--- src/controllers/activities.js
+++ src/controllers/activities.js
@@ -8,99 +8,119 @@ const { BadRequestError, NotFoundError } = require("../errors");
 // };
 
 const getActivity = async (req, res) => {
-  const {
-    user: { userId },
-    params: { id: activityId },
-  } = req;
-  const activity = await Activity.findOne({
-    _id: activityId,
-    createdBy: userId,
-  });
-  if (!activity) {
-    throw new NotFoundError(`No activity with id ${activityId}`);
+  try {
+    const {
+      user: { userId },
+      params: { id: activityId },
+    } = req;
+    const activity = await Activity.findOne({
+      _id: activityId,
+      createdBy: userId,
+    });
+    if (!activity) {
+      throw new NotFoundError(`No activity with id ${activityId}`);
+    }
+    res.status(StatusCodes.OK).json({ activity });
+  } catch (error) {
+    console.error(error);
+    throw new BadRequestError("Error in getActivity");
   }
-  res.status(StatusCodes.OK).json({ activity });
 };
 
 const createActivity = async (req, res) => {
-  req.body.createdBy = req.user.userId;
-  const activity = await Activity.create(req.body);
-  res.status(StatusCodes.CREATED).json({ activity });
+  try {
+    req.body.createdBy = req.user.userId;
+    const activity = await Activity.create(req.body);
+    res.status(StatusCodes.CREATED).json({ activity });
+  } catch (error) {
+    console.error(error);
+    throw new BadRequestError("Error in createActivity");
+  }
 };
 
 const editActivity = async (req, res) => {
-  const {
-    body: {
-      sportType,
-      address,
-      city,
-      zipCode,
-      state,
-      locationType,
-      date,
-      time,
-      listOfPlayers,
-      numberOfPlayers,
-      maxNumOfPlayers,
-      minNumOfPlayers,
-      hostName,
-      hostEmail,
-      hostPhoneNumber,
-      notes,
-      createdBy,
-    },
-    user: { userId },
-    params: { id: activityId },
-  } = req;
+  try {
+    const {
+      body: {
+        sportType,
+        address,
+        city,
+        zipCode,
+        state,
+        locationType,
+        date,
+        time,
+        listOfPlayers,
+        numberOfPlayers,
+        maxNumOfPlayers,
+        minNumOfPlayers,
+        hostName,
+        hostEmail,
+        hostPhoneNumber,
+        notes,
+        createdBy,
+      },
+      user: { userId },
+      params: { id: activityId },
+    } = req;
 
-  if (
-    sportType === "" ||
-    address === "" ||
-    city === "" ||
-    zipCode === "" ||
-    state === "" ||
-    locationType === "" ||
-    date === "" ||
-    time === "" ||
-    listOfPlayers === "" ||
-    numberOfPlayers === "" ||
-    maxNumOfPlayers === "" ||
-    minNumOfPlayers === "" ||
-    hostName === "" ||
-    hostEmail === "" ||
-    hostPhoneNumber === "" ||
-    notes === ""
-  ) {
-    throw new BadRequestError("Fields cannot be empty");
+    if (
+      sportType === "" ||
+      address === "" ||
+      city === "" ||
+      zipCode === "" ||
+      state === "" ||
+      locationType === "" ||
+      date === "" ||
+      time === "" ||
+      listOfPlayers === "" ||
+      numberOfPlayers === "" ||
+      maxNumOfPlayers === "" ||
+      minNumOfPlayers === "" ||
+      hostName === "" ||
+      hostEmail === "" ||
+      hostPhoneNumber === "" ||
+      notes === ""
+    ) {
+      throw new BadRequestError("Fields cannot be empty");
+    }
+    const activity = await Activity.findByIdAndUpdate(
+      {
+        _id: activityId,
+        createdBy: userId,
+      },
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (!activity) {
+      throw new NotFoundError(`No activity with id ${activityId}`);
+    }
+    res.status(StatusCodes.OK).json({ activity });
+  } catch (error) {
+    console.error(error);
+    throw new BadRequestError("Error in editActivity");
   }
-  const activity = await Activity.findByIdAndUpdate(
-    {
-      _id: activityId,
-      createdBy: userId,
-    },
-    req.body,
-    { new: true, runValidators: true }
-  );
-  if (!activity) {
-    throw new NotFoundError(`No activity with id ${activityId}`);
-  }
-  res.status(StatusCodes.OK).json({ activity });
 };
 
 const deleteActivity = async (req, res) => {
-  const {
-    user: { userId },
-    params: { id: activityId },
-  } = req;
+  try {
+    const {
+      user: { userId },
+      params: { id: activityId },
+    } = req;
 
-  const activity = await Activity.findByIdAndRemove({
-    _id: activityId,
-    createdBy: userId,
-  });
-  if (!activity) {
-    throw new NotFoundError(`No activity with id ${activityId}`);
+    const activity = await Activity.findByIdAndRemove({
+      _id: activityId,
+      createdBy: userId,
+    });
+    if (!activity) {
+      throw new NotFoundError(`No activity with id ${activityId}`);
+    }
+    res.status(StatusCodes.OK).send();
+  } catch (error) {
+    console.error(error);
+    throw new BadRequestError("Error in deleteActivity");
   }
-  res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
