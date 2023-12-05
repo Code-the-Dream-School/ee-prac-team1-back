79a17e667d1b14933eb664827a2bca0ab24734f0
Ljiljana J <ljiljajanjic@gmail.com>
2023-12-05 15:23:02 -0500

    toUpperCase transformation

--- src/utils/locationValidation.js
+++ src/utils/locationValidation.js
@@ -4,14 +4,20 @@ const xmlbuilder2 = require('xmlbuilder2');
 
 const createUSPSRequestXML = (location) => {
     const { address, city, state, zipCode } = location;
+    const uppercaseLocation = {
+        address: address.toUpperCase(),
+        city: city.toUpperCase(),
+        state: state.toUpperCase(),
+        zipCode: zipCode.toUpperCase()
+    };
     return xmlbuilder2.create({ version: '1.0' })
         .ele('AddressValidateRequest', { USERID: '6F83CODET2789' })
         .ele('Address')
-        .ele('Address1').txt(address).up()
+        .ele('Address1').txt(uppercaseLocation.address).up()
         .ele('Address2').txt().up()
-        .ele('City').txt(city).up()
-        .ele('State').txt(state).up()
-        .ele('Zip5').txt(zipCode).up()
+        .ele('City').txt(uppercaseLocation.city).up()
+        .ele('State').txt(uppercaseLocation.state).up()
+        .ele('Zip5').txt(uppercaseLocation.zipCode).up()
         .ele('Zip4').up()
         .up()
         .end({ prettyPrint: true });
@@ -32,42 +38,38 @@ const validateLocationWithUSPS = async (location) => {
             throw new Error(obj.AddressValidateResponse.Address.Error.Description);
         }
         const correctedCity = obj.AddressValidateResponse.Address.City;
-        if (correctedCity !== location.city) {
+        if (correctedCity.toUpperCase() !== location.city.toUpperCase()) {
             console.error('Error in validateLocationWithUSPS: Incorrect City');
-            throw new Error('Incorrect City');
+            throw new Error('Incorrect City input');
         }
         const correctedState = obj.AddressValidateResponse.Address.State;
-        if (correctedState !== location.state) {
+        if (correctedState.toUpperCase() !== location.state.toUpperCase()) {
             console.error('Error in validateLocationWithUSPS: Incorrect State');
-            throw new Error('Incorrect State');
+            throw new Error('Incorrect State input');
         }
 
         const correctedZip5 = obj.AddressValidateResponse.Address.Zip5;
         if (correctedZip5 !== location.zipCode) {
             console.error('Error in validateLocationWithUSPS: Incorrect ZipCode');
-            throw new Error('Incorrect ZipCode');
+            throw new Error('Incorrect ZipCode input');
         }
         return obj;
     } catch (error) {
         console.error('Error in validateLocationWithUSPS:', error);
 
+        let errorMessage;
         if (error.message === 'Incorrect City') {
-            throw new Error('Invalid city. Please check the city name.');
+            errorMessage = 'Invalid city. Please check the city name.';
         } else if (error.message === 'Incorrect State') {
-            throw new Error('Invalid state. Please check the state abbreviation.');
+            errorMessage = 'Invalid state. Please check the state abbreviation.';
         } else if (error.message === 'Incorrect ZipCode') {
-            throw new Error('Invalid ZIP code. Please check the ZIP code.');
+            errorMessage = 'Invalid ZIP code. Please check the ZIP code.';
         } else {
-            throw new Error('Error validating location with USPS');
+            errorMessage = 'Error validating location with USPS.';
         }
+        throw new Error(errorMessage + ' ' + error.message);
     }
 };
 
 
 module.exports = { validateLocationWithUSPS };
-
-
-
-
-
-
