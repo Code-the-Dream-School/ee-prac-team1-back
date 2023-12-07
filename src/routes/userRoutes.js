cd68ebf68767d1f754fc4955ed5957dde4b266f9
Vitalii Popovych <109319234+vitaliipp@users.noreply.github.com>
2023-12-07 17:57:11 -0600

    add user profile images

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authenticateUser = require('../middleware/authentication');
+const { upload } = require('../middleware/multer.js');
 
 const {
   getCurrentUser,
@@ -11,7 +12,12 @@ const {
 
 router.get('/current-user', authenticateUser, getCurrentUser);
 router.delete('/:userId', authenticateUser, deleteUserAccount);
-router.patch('/updateUser', authenticateUser, editUserProfile);
+router.patch(
+  '/updateUser',
+  upload.single('profileImage'),
+  authenticateUser,
+  editUserProfile
+);
 router.patch('/updateUserPassword', authenticateUser, updateUserPassword);
 
 module.exports = router;
